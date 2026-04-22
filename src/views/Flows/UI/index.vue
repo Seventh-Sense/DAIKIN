ffd8a8e3002328c4d1e679dca01d8135153c1237
Seventh-Sense <1577257077@qq.com>
2026-04-22 10:26:27 +0800

    modify config

--- src/views/Flows/UI/index.vue
+++ src/views/Flows/UI/index.vue
@@ -123,6 +123,9 @@ const handleDelete = (data: any) => {
   console.log("handleDelete", data);
 };
 
+let prduction = true;
+
+//http://192.168.10.85:9090/#/graphic/cc47b6d6-d1c8-410b-b8d7-02ad25bf64ef
 const handleEdit = (data: any) => {
   console.log("handleEdit", data);
   //设备IP
@@ -132,24 +135,35 @@ const handleEdit = (data: any) => {
   let name = window.location.hostname;
   let port = window.location.port;
 
-  if (data.type === "Dashboard") {
-    window.open(
-      `http://${name}:${port}/editor/#/chart/home/1/${IP}/${currentLocale}`,
-      "_blank",
-    );
-
-    // window.open(
-    //   `http://${name}:5173/#/chart/home/1/${IP}/${currentLocale}`,
-    //   "_blank",
-    // );
+  if (prduction) {
+    if (data.type === "Dashboard") {
+      window.open(
+        `http://${name}:${port}/editor/#/chart/home/1/${IP}/${currentLocale}`,
+        "_blank",
+      );
+    } else {
+      window.open(
+        `http://${name}:${port}/editor/#/graphic/2/${IP}/${currentLocale}`,
+        "_blank",
+      );
+    }
   } else {
-    window.open(
-      `http://${name}:${port}/editor/#/graphic/2/${IP}/${currentLocale}`,
-      "_blank",
-    );
+    if (data.type === "Dashboard") {
+      window.open(
+        `http://${name}:5173/#/chart/home/1/${IP}/${currentLocale}`,
+        "_blank",
+      );
+    } else {
+      window.open(
+        `http://${name}:5173/#/graphic/2/${IP}/${currentLocale}`,
+        "_blank",
+      );
+    }
   }
 };
 
+//http://192.168.10.85:9090/#/chart/preview/ac37153b-ebc0-4fae-847a-f0c4b1e2026f
+//http://192.168.10.85:9090/#/graphic/preview/cc47b6d6-d1c8-410b-b8d7-02ad25bf64ef
 const handlePreview = (data: any) => {
   console.log("handlePreview", data);
   const currentLocale = localeStore.currentLocale;
@@ -157,11 +171,6 @@ const handlePreview = (data: any) => {
 
   let name = window.location.hostname;
   let port = window.location.port;
-
-  let prduction = true;
-
-  //http://192.168.10.85:9090/#/chart/preview/ac37153b-ebc0-4fae-847a-f0c4b1e2026f
-  //http://192.168.10.85:9090/#/graphic/preview/cc47b6d6-d1c8-410b-b8d7-02ad25bf64ef
 
   if (prduction) {
     if (data.type === "Dashboard") {
